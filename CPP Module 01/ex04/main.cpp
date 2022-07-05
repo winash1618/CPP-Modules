e73e585714eab2ba8b9bdf911538577ec3dfb586
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-07-05 06:32:00 +0400

    ex04 completed

---
+++ CPP Module 01/ex04/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void read_and_replace(std::string filename, std::string find, std::string replace)
+{
+	std::ifstream file(filename);
+	if (file.is_open())
+	{
+		std::cout << "Opened successfully" << std::endl;
+	}
+	else
+	{
+		std::cout << "Error opening file" << std::endl;
+		_Exit(1);
+	}
+	std::string line;
+	std::string replace_filename = filename + ".replace";
+	std::ofstream replace_file(replace_filename);
+	std::size_t loc;
+	while (!file.eof())
+	{
+		std::getline (file, line);
+		loc = line.find(find);
+		while (loc != 0xFFFFFFFFFFFFFFFF)
+		{
+			
+			line.erase(loc, find.size());
+			line.insert(loc,replace);
+			loc = line.find(find);
+		}
+		replace_file << line;
+		if (!file.eof())
+			replace_file << "\n";
+	}
+	file.close();
+	replace_file.close();
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 4)
+		read_and_replace(argv[1], argv[2], argv[3]);
+	return 0;
+}
