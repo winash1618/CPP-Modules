1820abd93b619e8ba7fcfa35676be04c83029b3d
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-07-08 14:07:10 +0400

    ex01 added

---
+++ CPP Module 03/ex01/ClapTrap.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkaruvan <mkaruvan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/17 17:04:01 by mkaruvan          #+#    #+#             */
+/*   Updated: 2022/06/17 17:12:52 by mkaruvan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_H
+# define CLAPTRAP_H
+
+# include <iostream>
+
+class ClapTrap
+{
+	public:
+		ClapTrap(void);
+		ClapTrap(ClapTrap const & src);
+		ClapTrap & operator=(ClapTrap const & rhs);
+		~ClapTrap(void);
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+	private:
+		std::string _name;
+		int _hitPoints = 10;
+		int _energyPoints = 10;
+		int _attackDamage = 0;
+};
+
+#endif
