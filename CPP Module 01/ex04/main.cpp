67a1b538cf69ae847328a674c5c074f29ba3209f
Muhammed Asharaf K V <51042907+winash1618@users.noreply.github.com>
2022-08-21 11:41:05 +0530

    CPP01 - ex04 : reading error

--- CPP Module 01/ex04/main.cpp
+++ CPP Module 01/ex04/main.cpp
@@ -8,7 +8,7 @@ void read_and_replace(std::string filename, std::string find, std::string replac
 	std::ifstream file(filename);
 	if (file.is_open())
 	{
-		std::cout << "Opened successfully" << std::endl;
+		std::cout << "File opened successfully" << std::endl;
 	}
 	else
 	{
@@ -16,21 +16,31 @@ void read_and_replace(std::string filename, std::string find, std::string replac
 		exit(0);
 	}
 	std::string line;
+	std::string line2;
+
 	std::string replace_filename = filename + ".replace";
 	std::ofstream replace_file(replace_filename);
 	std::size_t loc;
-	while (!file.eof())
+	while (!file.eof() && find.size())
 	{
+		line2 = "";
+		std::cout << "hi" << std::endl;
 		std::getline (file, line);
 		loc = line.find(find);
 		while (loc != 0xFFFFFFFFFFFFFFFF)
 		{
-			
 			line.erase(loc, find.size());
-			line.insert(loc,replace);
+			line2 = line2 + line.substr(0, loc);
+			line2 = line2 + replace;
+			line = line.substr (loc);
 			loc = line.find(find);
+			std::cout << "hi: " << loc << " " << line.size() << std::endl;
 		}
-		replace_file << line;
+		if (line.size() && loc == 0xFFFFFFFFFFFFFFFF)
+		{
+			line2 = line2 + line.substr(0, loc);
+		}
+		replace_file << line2;
 		if (!file.eof())
 			replace_file << "\n";
 	}
@@ -42,5 +52,11 @@ int main(int argc, char **argv)
 {
 	if (argc == 4)
 		read_and_replace(argv[1], argv[2], argv[3]);
+	else
+	{
+		std::cout << "Wrong number of arguements" << std::endl;
+		std::cout << "Please follow the format below" << std::endl;
+		std::cout << "./main <filename> <word1> <word2>" << std::endl;
+	}
 	return 0;
 }
